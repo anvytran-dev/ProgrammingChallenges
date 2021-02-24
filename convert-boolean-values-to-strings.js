2183017b858e6c82116ecb1b8845b6e23e5fc31f
Anvy Tran <anvytrandev@gmail.com>
2021-02-24 17:09:30 -0500

    Code Wars: Convert Boolean Values to Strings 'Yes' or 'No'

---
+++ convert-boolean-values-to-strings.js
@@ -0,0 +1,12 @@
+//P: The parameter is the variable that will take the boolean value.
+//R: Return either the "Yes" or "No" string based on the condition. 
+//E: If the boolean value is true, return "yes."
+//P: If bool value is true, return "yes." If bool value is false, return "no."
+function boolToWord(bool){
+  
+  if (bool != true){
+       return "No"   
+  } else if (bool = true){
+    return "Yes"
+  }
+}
