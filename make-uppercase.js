9fb60720c58f058ec51ca87de680e7adcf7830d4
Anvy Tran <anvytrandev@gmail.com>
2021-02-26 23:34:16 -0500

    Code Wars: MakeUpperCase

---
+++ make-uppercase.js
@@ -0,0 +1,10 @@
+//P: The paramenter is a variable holding a string.
+//R: Return the string in uppercase.
+//E: If string is 'jump,' return 'JUMP.'
+//P: Use the .toUpperCase(); method to conver the stirng to uppercase.
+
+function makeUpperCase(str) {
+  
+  return str.toUpperCase();
+  
+}
