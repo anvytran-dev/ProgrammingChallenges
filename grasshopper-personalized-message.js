d9acb2f6605020904cd89c78375002f5f72d100c
Anvy Tran <anvytrandev@gmail.com>
2021-03-05 00:31:53 -0500

    Code Wars: Grasshopper-Personalized Message

---
+++ grasshopper-personalized-message.js
@@ -0,0 +1,8 @@
+//P: There are 2 parameters: name and owner. 
+//Return 'Hello boss,' if name and owner are equal. If not, return 'Hello guest.'
+//E: ( Bob, Joe ). 'Hello guest'
+//Use conditional statement. 
+
+function greet ( name, owner ) {
+  return name === owner ? 'Hello boss' : 'Hello guest'
+}
