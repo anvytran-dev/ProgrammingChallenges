60a958bc221aebefecbb9fbd1f75b0ff7a7ef8fc
Anvy Tran <anvytrandev@gmail.com>
2021-03-06 01:51:11 -0500

    Code Wars: My head is at the wrong end

---
+++ my-head-is-at-the-wrong-end.js
@@ -0,0 +1,7 @@
+//P: There is one parameter: an array.
+//R: Return the array with the elements in reversed order.
+//E: [1, 2, 3] => [3, 2, 1]
+//Use the reverse method on the array. 
+function fixTheMeerkat(arr) {
+ return arr.reverse();
+}
