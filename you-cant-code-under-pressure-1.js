772f342a4f8d8d1187234720b263e71742b533ae
Anvy Tran <anvytrandev@gmail.com>
2021-03-07 20:07:44 -0500

    Code Wars: You Can't Code Under Pressure #1

---
+++ you-cant-code-under-pressure-1.js
@@ -0,0 +1,8 @@
+//P: There is one paramter: i. i is an integer.
+//R: Return double the integer.
+//E: i = 3, => 6
+//P: Use an arithmetic operator to double the integer. 
+function doubleInteger(i) {
+  
+  return i *= 2;
+}
