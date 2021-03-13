a3b8a1c3b33909c15a5c025453a8aeffd313482d
Anvy Tran <anvytrandev@gmail.com>
2021-03-13 00:32:37 -0500

    Code Wars: Convert a Number to a String

---
+++ convert-a-number-to-a-string.js
@@ -0,0 +1,7 @@
+//P: There is one parameter: integer named 'num'.
+//R: Return the integer in string format.
+//E: num = 7; => '7'
+//P: Use the .toString()method to convert the integer into a string. 
+function numberToString(num) {
+  return num.toString();
+}
