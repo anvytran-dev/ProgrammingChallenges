2e66eae1151da014fbffaa029a7c5aeccc0242fa
Anvy Tran <anvytran857@gmail.com>
2021-03-20 21:04:37 -0400

    Code Wars: Return Negative

---
+++ return-negative.js
@@ -0,0 +1,7 @@
+//P: There is one parameter: num.
+//R: Return the number in its negative form.
+//E: 4 -> -4
+//P: Use the -Math object and the .abs() method.
+function makeNegative(num) {
+  return -Math.abs(num)
+}
