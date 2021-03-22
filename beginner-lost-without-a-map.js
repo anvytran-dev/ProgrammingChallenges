85ffb5317214baab12260598f01b608840b77e67
Anvy Tran <anvytran857@gmail.com>
2021-03-21 23:58:13 -0400

    Code Wars: Beginner Lost without a map

---
+++ beginner-lost-without-a-map.js
@@ -0,0 +1,13 @@
+//P: There is one parameter. The parameter is an array of integers.
+//R: Return a new array with each value doubled from the original array.
+//E: [2, 3, 4] --> [4, 6, 8]
+//P: Use the .map(); method to create a new array that is populated with the results of calling a function that doubles the value of every element in the calling array. 
+
+
+
+function maps(x) {
+  
+  return x.map(x => x * 2)
+  
+  
+}
