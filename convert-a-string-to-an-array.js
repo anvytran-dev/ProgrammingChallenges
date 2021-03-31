03b1c12ea6622684ce0e449b1e90e27ecece58e5
Anvy Tran <anvytran857@gmail.com>
2021-03-31 14:09:54 -0400

    Code Wars: Convert a string to an array

---
+++ convert-a-string-to-an-array.js
@@ -0,0 +1,9 @@
+//P: There is one parameter: a string. 
+//R: Return an array of words.
+//E: "Robin Singh" => ["Robin", "Singh"]
+//P: Use the split method to convert the string to an array. For the seperator, use " " to get words.
+function stringToArray(string){
+
+  return string.split(" ")
+
+}
