fac225fe85eceb344685ca5e741ecdafbd380f89
Anvy Tran <anvytran857@gmail.com>
2021-04-03 23:38:44 -0400

    Thinkful-String Drills: Repeater

---
+++ thinkful-string-drills-repeater.js
@@ -0,0 +1,7 @@
+//P: There are 2 parameters: string (which is a string) and n (which is a number).
+//R: Return the string repeated however many times n is. 
+//E: string ==='a', n === 5 => 'aaaaa'
+//P: Use the repeat string method. This method takes a number (the number of times you want the stirng repeated).
+function repeater(string, n){
+  return string.repeat(n)
+}
