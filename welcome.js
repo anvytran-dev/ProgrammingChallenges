178df8adcefe03e796337c55b9713a76ef827fba
Anvy Tran <anvytran857@gmail.com>
2021-05-07 23:51:43 -0400

    Code Wars: Welcome

---
+++ welcome.js
@@ -0,0 +1,31 @@
+function greet(language) {
+  let langObj ={
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+    IP_ADDRESS_INVALID:  'Welcome',
+    IP_ADDRESS_NOT_FOUND: 'Welcome',
+    IP_ADDRESS_REQUIRED: 'Welcome'
+  }
+  
+   for(const [key, value] of Object.entries(langObj)){
+     if(key == language) {
+       return value
+     }
+
+   }
+}
