dc7f48a19ec8330b36350da11e887998d3fc087e
Anvy Tran <anvytran857@gmail.com>
2021-05-13 14:32:51 -0400

    happy-number

---
+++ happy-number.js
@@ -0,0 +1,30 @@
+// Write an algorithm to determine if a number n is happy.
+// A happy number is a number defined by the following process:
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+
+let arr = []
+
+function isHappy(num) {
+ 
+  let sum = num.toString().split("").reduce((a,c) => a += Math.pow(c, 2), 0) 
+
+  if(arr.indexOf(sum) != -1){
+    return false
+  }
+
+    arr.push(sum)
+
+    if(sum == 1) {
+      return true
+    } 
+
+    if(sum != 1){
+      return isHappy(sum)
+    }
+    
+}
+
